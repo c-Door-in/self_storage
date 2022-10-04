d79065481a429e88fdfcd8dd85e80ff2cca6070e
ilyashirko <ilyashirko@gmail.com>
2022-10-04 19:09:54 +0300

    replace templates, fix static on boxes, add favi

--- self_storage/urls.py
+++ self_storage/urls.py
@@ -2,9 +2,15 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
+from store import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.index),
+    path('boxes/', views.boxes),
+    path('my_rent/', views.my_rent),
+    path('my_rent_empty/', views.my_rent_empty),
+    path('faq/', views.faq),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- store/views.py
+++ store/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def index(request):
+
+    return render(request, 'index.html')
+
+
+def boxes(request):
+
+    return render(request, 'boxes.html')
+
+
+def my_rent(request):
+
+    return render(request, 'my_rent.html')
+
+
+def my_rent_empty(request):
+
+    return render(request, 'my_rent_empty.html')
+
+
+def faq(request):
+
+    return render(request, 'faq.html')
