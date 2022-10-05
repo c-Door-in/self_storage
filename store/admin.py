e383e3fb9915831e85e9469af4c61452ef13ba24
Atmoslayer <ddsomdim@gmail.com>
2022-10-05 22:14:19 +0300

    Models created

--- store/admin.py
+++ store/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Application, Customer, Storage, Box
 
-# Register your models here.
+admin.site.register(Application)
+admin.site.register(Customer)
+admin.site.register(Storage)
+admin.site.register(Box)
+
+
--- store/models.py
+++ store/models.py
@@ -1,3 +1,48 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Application(models.Model):
+    email_adress = models.EmailField(default='Почта пользователя', verbose_name='Почта пользователя')
+
+    def __str__(self):
+        return self.email_adress
+
+
+class Customer(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, null=True)
+    photo = models.ImageField(verbose_name='Фото пользователя')
+    phone_number = models.CharField(null=True, blank=True, max_length=10, verbose_name='Телефон пользователя')
+
+    def __str__(self):
+        return f'{self.user.first_name} {self.user.last_name}'
+
+
+class Storage(models.Model):
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    location_city = models.CharField(default='Москва', max_length=35, verbose_name='Город')
+    location_street_name = models.CharField(default='Тверская', max_length=35, verbose_name='Улица')
+    location_street_number = models.CharField(default='15', max_length=10, verbose_name='Идентификатор дома')
+    store_temperature = models.DecimalField(verbose_name='Температура на складе', decimal_places=2, max_digits=5)
+    ceiling_height = models.DecimalField(verbose_name='Высота потолка', decimal_places=2, max_digits=5)
+    payment_per_month = models.DecimalField(verbose_name='Оплата за месяц', decimal_places=2, max_digits=10)
+    photo = models.ImageField(blank=True, verbose_name='Фото')
+    note = models.CharField(null=True, blank=True, max_length=35, verbose_name='Заметка')
+
+    def __str__(self):
+        return f'Склад {self.id}'
+
+
+class Box(models.Model):
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE, verbose_name='Клиент')
+    storage = models.ForeignKey(Storage, on_delete=models.CASCADE, verbose_name='Склад')
+    number = models.CharField(max_length=20, verbose_name='Номер в базе данных')
+    is_use = models.BooleanField(default=False, verbose_name='Занятость склада')
+    rental_start_time = models.DateTimeField(null=True, verbose_name='Время начала аренды')
+    rental_end_time = models.DateTimeField(null=True, verbose_name='Время окончания аренды')
+    level = models.IntegerField(verbose_name='Этаж')
+    square = models.IntegerField(verbose_name='Площадь')
+    price = models.DecimalField(verbose_name='Цена', decimal_places=4, max_digits=10)
+
+    def __str__(self):
+        return f'Бокс {self.number} склада {self.storage.id}'
