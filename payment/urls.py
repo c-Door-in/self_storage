c65dd957bd93f72671d570ba61448ae599e77066
c_Door in <egor1@mail.ru>
2022-10-10 03:28:09 +0300

    Save payment summary to db

--- payment/urls.py
+++ payment/urls.py
@@ -7,5 +7,5 @@ app_name = 'payment'
 urlpatterns = [
     path('success/', views.pay_success, name='successed_payment'),
     path('cancelled/', views.cancelled, name='cancelled_payment'),
-    path('', views.make_payment, name='make_payment'),
+    path('<int:box_pk>', views.make_payment, name='make_payment'),
 ]
--- payment/views.py
+++ payment/views.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+from datetime import datetime, timedelta
 
 import stripe
 from django.shortcuts import redirect, render
@@ -6,17 +6,17 @@ from django.urls import reverse
 from django.views.generic import TemplateView
 from django.conf import settings
 from django.contrib.auth.decorators import login_required
-from store.models import Box
+from store.models import Box, Customer
 from django.contrib.auth.models import User
 from django.urls import reverse_lazy
 
 
-def make_payment(request):
+def make_payment(request, box_pk):
     user_id = request.user.id
-    print(user_id)
+    box = Box.objects.get(pk=box_pk)
     stripe.api_key = settings.STRIPE_API_KEY
 
-    amount =  5000
+    amount =  int(box.price)*100
 
     session = stripe.checkout.Session.create(
         payment_method_types=['card'],
@@ -24,13 +24,17 @@ def make_payment(request):
             'price_data': {
                 'currency': 'rub',
                 'product_data': {
-                    'name': f'Ваш заказ №',
+                    'name': f'Вы арендуете бокс по адресу {box.storage.location_street_name}, {box.storage.location_street_number}',
                 },
                 'unit_amount': amount,
             },
             'quantity': 1,
         }],
         mode='payment',
+        metadata = {
+            'user_id': user_id,
+            'box_pk': box_pk,
+        },
         success_url=request.build_absolute_uri(reverse('payment:successed_payment')),
         cancel_url=request.build_absolute_uri(reverse('payment:cancelled_payment')),
     )
@@ -39,13 +43,20 @@ def make_payment(request):
 
 
 def pay_success(request):
+    stripe.api_key = settings.STRIPE_API_KEY
+    stripe_sessions = stripe.checkout.Session.list(limit=3)
+    session = stripe_sessions['data'][0]
+    if session['payment_status'] == 'paid':
+        user_id = session['metadata']['user_id']
+        box_pk = session['metadata']['box_pk']
+        customer, created = Customer.objects.get_or_create(user__id=user_id)
+        box = Box.objects.get(pk=box_pk)
+        box.customer = customer
+        box.in_use = True
+        box.rental_start_time = datetime.now()
+        box.rental_end_time = datetime.now() + timedelta(days=30)
+        box.save()
     user_id = request.user.id
-    # sub_type = order['sub_type']
-    print(user_id)
-    print(request)
-    print(request.session)
-
-    # del request.session[f'sub_{str(user_id)}']
 
     return render(request, 'success.html')
 
